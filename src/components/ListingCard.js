f59fc2a15296861d66a5bd2afb38b3dc48014c9c
Chang Shin <cshin7374@gmail.com>
2022-07-29 22:19:40 -0500

    commit tree 654170e3ea4b30cf9d4b437efe93c3867800e240

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function ListingCard() {
+function ListingCard({id, description, image, location, onHandleDelete}) {
+  function handleClick () {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+    .then(r=>r.json())
+    .then(()=> onHandleDelete(id))
+  }
+  
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={"description"} />
       </div>
       <div className="details">
         {true ? (
@@ -13,9 +21,9 @@ function ListingCard() {
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button className="emoji-button delete" onClick={handleClick}>🗑</button>
       </div>
     </li>
   );
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+  useEffect(()=> {
+    fetch("http://localhost:6001/listings")
+    .then(r=>r.json())
+    .then(listings=>setListings(listings))
+  },[])
+
+  function handleDelete(id) {
+    const deletedListings = listings.filter(listing=> listing.id !== id)
+    setListings(deletedListings)
+  }
+
+  const listingCards = listings.map(listing => {
+    return (
+      <ListingCard 
+        key={listing.id}
+        id={listing.id}
+        description={listing.description} 
+        image={listing.image}
+        location={listing.location}
+        onHandleDelete={handleDelete}
+      />
+    )
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
